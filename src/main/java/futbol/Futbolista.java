2f9bea1b121d0929e51b00fb0c51060108f8ef22
ChewCEC <camilo.echeverri.100@gmail.com>
2022-10-16 21:56:54 -0500

    Commit 8

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -61,7 +61,7 @@ public abstract class Futbolista implements Comparable {
     }
 
     public boolean equals(Futbolista f) {
-        return this.nombre.equals(f.nombre) && this.edad == f.edad && this.posicion.equals(f.posicion);
+        return this == f;
     }
 
     public abstract boolean jugarConLasManos();
